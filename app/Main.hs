44eb0be4df73c219f2b70c58386bdb79e70e3004
Levi Butcher <levibutcher355@gmail.com>
2024-01-03 20:11:10 -0500

    Setup scalpel

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main (main) where
 
 import Lib
+import Text.HTML.Scalpel (fetchTags)
+import Text.HTML.TagSoup (Tag)
+
+site = "https://clutchmov.com/"
 
 main :: IO ()
-main = someFunc
+main = do
+  tags <- fetchTags site :: IO [Tag String]
+  print tags
