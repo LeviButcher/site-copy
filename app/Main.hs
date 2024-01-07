8bde8291208d64d24a3451df50b8a7f5a745cbba
Levi Butcher <levibutcher355@gmail.com>
2024-01-06 20:02:00 -0500

    Save

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Lib
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Data.Maybe (fromMaybe)
+import Lib (downloadPage, saveFile)
+import Network.HTTP.Simple
+import Network.URI (parseAbsoluteURI, parseRelativeReference, parseURI, parseURIReference, pathSegments, uriPath)
 import Text.HTML.Scalpel (fetchTags)
 import Text.HTML.TagSoup (Tag)
 
-site = "https://clutchmov.com/"
+site :: URL
+site = "https://clutchmov.com"
+
+saveDir :: FilePath
+saveDir = "site"
+
+-- tags <- fetchTags site :: IO [Tag String]
+
+type URL = String
+
+type Dir = String
+
+getFilePath :: Dir -> URL -> String
+getFilePath dir url = dir <> "/" <> rPath <> ".html"
+  where
+    possibleURI = case parseURI url of
+      Just x -> if not . null . pathSegments $ x then Just x else Nothing
+      Nothing -> Nothing
+    rPath = maybe "index" uriPath possibleURI
 
 main :: IO ()
 main = do
-  tags <- fetchTags site :: IO [Tag String]
-  print tags
+  body <- downloadPage site
+  let fPath = getFilePath saveDir site
+  print fPath
+  saveFile body fPath
+  --
+  print "end"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
 module Lib
-    ( someFunc
-    ) where
+  ( saveFile,
+    downloadPage,
+  )
+where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Network.HTTP.Simple
+import System.Directory (createDirectoryIfMissing)
+import System.FilePath (takeDirectory)
+
+-- "./temp/sec/temp.txt"
+-- "temp/sec/temp.txt"
+saveFile :: FilePath -> String -> IO ()
+saveFile cont path = do
+  let dir = takeDirectory path
+  createDirectoryIfMissing True dir
+  writeFile path cont
+
+downloadPage :: String -> IO String
+downloadPage s = do
+  initReq <- parseRequest s
+  response <- httpLBS initReq
+  let body = L8.unpack . getResponseBody $ response
+  pure body
